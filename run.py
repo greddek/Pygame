280fc0a6d428657d3a60236104382468a3154d0b
LJ <79zoro@gmail.com>
2018-12-19 21:19:07 +0100

    Action

--- run.py
+++ run.py
@@ -2,3 +2,8 @@ import pygame
 
 resolutionDisplayWindow = (1280, 720)
 windowScreen = pygame.display.set_mode(resolutionDisplayWindow)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
