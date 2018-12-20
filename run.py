907a847df3898b07db649536c4ced4a26d5b92d2
LJ <79zoro@gmail.com>
2018-12-20 15:57:42 +0100

    Ticking

--- run.py
+++ run.py
@@ -6,9 +6,16 @@ import pygame, sys
 
 # initialization for modules ex. clock
 pygame.init()
-resolutionDisplayWindow = (1280, 720)
-windowScreen = pygame.display.set_mode(resolutionDisplayWindow)
-boxObject = pygame.Rect(10, 50, 200, 100)
+resolutionWindow    = (1280, 720)
+deltaTime           = 0.0
+convertForSeconds   = 1000.0
+maxTimePerSecond    = 70.0
+NormalizationTime   = 1 / maxTimePerSecond
+boxObject           = pygame.Rect(10, 50, 200, 100)
+windowScreen        = pygame.display.set_mode(resolutionWindow)
+# object clock tick for delay
+clockDelay = pygame.time.Clock()
+
 
 while True:
     # Handle events
@@ -17,15 +24,20 @@ while True:
             sys.exit(0)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
-    # Steering of the object
-    if pygame.key.get_pressed()[pygame.K_d]:
-        boxObject.x += 1
-    elif pygame.key.get_pressed()[pygame.K_s]:
-        boxObject.y += 1
-    elif pygame.key.get_pressed()[pygame.K_w]:
-        boxObject.y -= 1
-    elif pygame.key.get_pressed()[pygame.K_a]:
-        boxObject.x -= 1
+    # Ticking normalization for standard move on all processors
+    deltaTime += (clockDelay.tick() / convertForSeconds)
+    while deltaTime > NormalizationTime:
+        deltaTime -= NormalizationTime
+        # Steering of the object
+        if pygame.key.get_pressed()[pygame.K_d]:
+            boxObject.x += 1
+        elif pygame.key.get_pressed()[pygame.K_s]:
+            boxObject.y += 1
+        elif pygame.key.get_pressed()[pygame.K_w]:
+            boxObject.y -= 1
+        elif pygame.key.get_pressed()[pygame.K_a]:
+            boxObject.x -= 1
+
     # Refresh and draw new object
     windowScreen.fill((0, 0, 0))
     pygame.draw.rect(windowScreen, (0, 150, 255), boxObject)
